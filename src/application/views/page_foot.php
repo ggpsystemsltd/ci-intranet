3d7804cd0abf261eed0524c1d9f2d4df73a0d888
Murray Crane <murray.crane@ggpsystems.co.uk>
2017-02-14 16:36:34 +0000

    Redundant files

--- src/application/views/page_foot.php
+++
@@ -1,9 +0,0 @@
-<div class="address">
-	<address>Author: [ {author_mailto} ]</address>
-	<address>Copyright &copy; <?php echo date('o'); ?> GGP Systems Ltd</address>
-	<address>Page rendered in <?php echo $this->benchmark->elapsed_time(); ?>
-		seconds; <?php echo date('c'); ?></address>
-	<address>Remote IP: {remote_ip}</address>
-</div> <!-- address -->
-</body>
-</html>
